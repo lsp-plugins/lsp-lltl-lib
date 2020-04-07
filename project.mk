2e7faae81fc417c9a2168dce6214412523e987b7
SadKo <lsp.plugin@gmail.com>
2020-04-07 12:22:28 +0300

    Created skeleton

---
+++ project.mk
@@ -0,0 +1,13 @@
+# Package version
+ARTIFACT_NAME               = lsp-lltl-lib
+ARTIFACT_VARS               = LSP_LLTL_LIB
+ARTIFACT_HEADERS            = lsp-plug.in
+VERSION                     = 0.5.0-devel
+
+# List of dependencies
+TEST_DEPENDENCIES = \
+  LSP_TEST_FW
+
+DEPENDENCIES = \
+  STDLIB \
+  LSP_COMMON_LIB
