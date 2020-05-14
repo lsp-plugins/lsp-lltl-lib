d4b467786dd154da9d95922ff1b063f85e3ce88e
sadko4u <sadko4u@gmail.com>
2020-05-14 23:02:25 +0300

    Updated build scripts

--- include/lsp-plug.in/lltl/version.h
+++ include/lsp-plug.in/lltl/version.h
@@ -12,5 +12,16 @@
 #define LSP_LLTL_LIB_MINOR      5
 #define LSP_LLTL_LIB_MICRO      2
 
+#ifdef LSP_LLTL_LIB_BUILTIN
+    #define LSP_LLTL_LIB_EXPORT
+    #define LSP_LLTL_LIB_CEXPORT
+    #define LSP_LLTL_LIB_IMPORT
+    #define LSP_LLTL_LIB_CIMPORT
+#else
+    #define LSP_LLTL_LIB_EXPORT         LSP_SYMBOL_EXPORT
+    #define LSP_LLTL_LIB_CEXPORT        LSP_CSYMBOL_EXPORT
+    #define LSP_LLTL_LIB_IMPORT         LSP_SYMBOL_IMPORT
+    #define LSP_LLTL_LIB_CIMPORT        LSP_CSYMBOL_IMPORT
+#endif
 
 #endif /* LSP_PLUG_IN_LLTL_VERSION_H_ */
