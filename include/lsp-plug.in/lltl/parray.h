65359bad145b63ac8fc4069893e458be1de15792
sadko4u <sadko4u@gmail.com>
2020-05-21 00:44:00 +0300

    Fixed upget() method of lltl::parray

--- include/lsp-plug.in/lltl/parray.h
+++ include/lsp-plug.in/lltl/parray.h
@@ -130,7 +130,7 @@ namespace lsp
                     inline T **get_n(size_t idx, size_t n, T **x)                   { return pcast(v.get_n(idx, n, vcast(x)));     }
                     inline T **pget(size_t idx)                                     { return (idx < v.nItems) ? pcast(&v.vItems[idx]) : NULL;       }
                     inline T *uget(size_t idx)                                      { return cast(v.vItems[idx]);           }
-                    inline T *upget(size_t idx)                                     { return pcast(&v.vItems[idx]);         }
+                    inline T **upget(size_t idx)                                    { return pcast(&v.vItems[idx]);         }
                     inline T *first()                                               { return (v.nItems > 0) ? cast(v.vItems[0]) : NULL; }
                     inline T *last()                                                { return (v.nItems > 0) ? cast(v.vItems[v.nItems - 1]) : NULL;  }
                     inline T **array()                                              { return pcast(v.vItems);               }
