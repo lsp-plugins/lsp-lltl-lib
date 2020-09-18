5f4b1c0fd3f36e7a42a5b9eba763afdfe657a508
sadko4u <sadko4u@gmail.com>
2020-09-18 22:43:03 +0300

    Build changes
    * Updated build scripts that now use tags without prefixes first.

--- project.mk
+++ project.mk
@@ -1,9 +1,30 @@
+#
+# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+#
+# This file is part of lsp-lltl-lib
+#
+# lsp-lltl-lib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# lsp-lltl-lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lsp-lltl-lib.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Package version
 ARTIFACT_NAME               = lsp-lltl-lib
+ARTIFACT_DESC               = Low-Level Template Library for C++
 ARTIFACT_VARS               = LSP_LLTL_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
-ARTIFACT_VERSION            = 0.5.3-devel
+ARTIFACT_VERSION            = 0.5.3
 
 # List of dependencies
 TEST_DEPENDENCIES = \
