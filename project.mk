85126885d7bbce863239735469ed56f1253a341a
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-03-05 01:56:19 +0300

    Version up

--- project.mk
+++ project.mk
@@ -23,4 +23,4 @@ ARTIFACT_ID                 = LSP_LLTL_LIB
 ARTIFACT_NAME               = lsp-lltl-lib
 ARTIFACT_DESC               = Low-Level Template Library for C++
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.17
+ARTIFACT_VERSION            = 1.0.18-devel
